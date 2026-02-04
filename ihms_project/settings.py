fac3e41127c1c95a10bd5112784905986cf9d158
sathwik-89 <147127984+sathwik-89@users.noreply.github.com>
2026-02-04 12:13:09 +0530

    allowed host error

--- ihms_project/settings.py
+++ ihms_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ldv%vwcw%uk38r_rxdw#8s4)x!ne!s1!hvjc__x3h@0f68l*!9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
